92c34bd054df8bf889a0b870fbfaed9019f949de
kluhan <klaus.luhan@gmail.com>
2025-12-03 03:43:05 +0100

    fix

--- modules/security.nix
+++ modules/security.nix
@@ -9,14 +9,15 @@
 
   services.fail2ban.enable = true;
 
-
   # Hardened SSH defaults for all hosts
   services.openssh = {
     enable = true;
     KexAlgorithms = [ "curve25519-sha256" ];
     ciphers = [ "chacha20-poly1305@openssh.com" ];
+
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
+    };
   };
 }
