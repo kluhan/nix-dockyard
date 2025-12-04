76398e7f60b9b4d3455154db2a5d36f1df823b24
kluhan <klaus.luhan@gmail.com>
2025-12-04 02:03:55 +0100

    Clone NixOS config and generate hardware configuration
    Clone the NixOS dockyard repository and generate hardware configuration. Adjust permissions for the generated file.

--- generate-hardware-config.sh
+++ generate-hardware-config.sh
@@ -1,24 +1,11 @@
 set -euo pipefail
 
+# Clone the configuration
+git clone https://github.com/kluhan/nixos-dockyard.git /mnt/etc/nixos
+
 # Generate hardware configuration for the target system
-nixos-generate-config --root /mnt
-
-# Remove the default configuration file if it exists
-if [ -f /mnt/etc/nixos/configuration.nix ]; then
-    rm /mnt/etc/nixos/configuration.nix
-fi
-
-# Move hardware-configuration.nix to the local project directory
-if [ -f /mnt/etc/nixos/hardware-configuration.nix ]; then
-    mv /mnt/etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
-else
-    echo "Error: hardware-configuration.nix not found!"
-    exit 1
-fi
+nixos-generate-config --show-hardware-config >> /mnt/etc/nixos/hardware-configuration.nix
 
 # Ensure correct permissions
 chown nixos hardware-configuration.nix
 chgrp users hardware-configuration.nix
-
-# Add hardware generationt to git so the flake can see it
-git add hardware-configuration.nix
