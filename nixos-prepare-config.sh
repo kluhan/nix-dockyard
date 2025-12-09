5cd09bbd6ef611dfa31cb637ce7b1d0b2d4dc652
kluhan <klaus.luhan@gmail.com>
2025-12-09 20:29:05 +0100

    Fix repository URL in nixos-prepare-config.sh

--- nixos-prepare-config.sh
+++ nixos-prepare-config.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
 
 # Clone the configuration
-git clone https://github.com/kluhan/nixos-dockyard.git /mnt/etc/nixos
+git clone https://github.com/kluhan/nix-dockyard.git /mnt/etc/nixos
 
 # Generate hardware configuration for the target system
 nixos-generate-config --show-hardware-config >> /mnt/etc/nixos/hardware-configuration.nix
