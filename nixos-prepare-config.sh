f07d5066a5c7cd6adc16ae23d8ffa2a29a78b617
kluhan <klaus.luhan@gmail.com>
2025-12-09 20:32:29 +0100

    Fix nixos-generate-config output redirection
    Redirect output of nixos-generate-config to overwrite the file instead of appending.

--- nixos-prepare-config.sh
+++ nixos-prepare-config.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 git clone https://github.com/kluhan/nix-dockyard.git /mnt/etc/nixos
 
 # Generate hardware configuration for the target system
-nixos-generate-config --show-hardware-config >> /mnt/etc/nixos/hardware-configuration.nix
+nixos-generate-config --show-hardware-config > /mnt/etc/nixos/hardware-configuration.nix
 
 # Ensure correct permissions
 chown nixos hardware-configuration.nix
